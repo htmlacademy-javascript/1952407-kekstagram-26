5c3a609957520437bc879591f2f3d5ee0a30842c
Vladimir <vokar90@yandex.ru>
2022-06-16 00:40:43 +0300

    add random number array function

--- js/main.js
+++ js/main.js
@@ -1,31 +1,134 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно
-// Функция взята по ссылке https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Функцию принес Кекс
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+function getRandomPositiveInteger(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
 
-  if (max < 0 || min < 0) {
-    return -1;
-  }
+// Функция для проверки максимальной длины строки Кекстаграм
 
-  if (max <= min) {
-    return null;
-  }
-
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+function checkStringLength(string, length) {
+  return string.length <= length;
 }
 
 // eslint-disable-next-line no-console
-console.log(getRandomIntInclusive(0, -11));
+// console.log(checkStringLength('Some comment', 140));
 
 
-//Функция для проверки максимальной длины строки Кекстаграм
+// Задание 4. Больше деталей
 
-function checkCommentLength(checkComment, maxLength) {
-  return checkComment.length <= maxLength;
-}
+const userNames = [
+  'Артемий',
+  'Дарья',
+  'Борис',
+  'Анна',
+  'Яна',
+  'Алина',
+  'Матвей',
+  'Виктория',
+  'Тимофей',
+  'Тимур',
+  'Эмилия',
+  'Маргарита',
+  'Илья',
+  'Михаил',
+  'Кристина',
+  'Святослав',
+  'Владислав',
+  'Всеволод',
+  'Егор',
+  'Анатолий',
+  'Александр',
+  'Константин',
+  'Иван',
+  'Тихон',
+  'Екатерина'
+];
+
+const userDescriptions = [
+  'Я кушаю',
+  'Я и мои друзья',
+  'Моя будущая жена',
+  'Круто потусили',
+  'Я тут спал',
+  'Говорят js это весело'
+];
+
+const userComments = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const getRandomArrayElement = (array) => array[getRandomPositiveInteger(0, array.length - 1)];
+
+// const getSentencesNumber = () => {}; придумать функцию получения 1 или 2 не повторяющихся предложений.
+
+const generateComment = () => ({
+  'id': '',
+  'avatar': `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+  'message': '',
+  'name': getRandomArrayElement(userNames),
+});
+
+const generatePhoto = () => ({
+  'id': '',
+  'url': `photos/.jpg`,
+  'description': getRandomArrayElement(userDescriptions),
+  'likes': getRandomPositiveInteger(15, 200),
+  'comments': ''
+});
+
+const objectsPhotos = Array.from({ length: 4 }, generatePhoto);
 
 // eslint-disable-next-line no-console
-console.log(checkCommentLength('Some comment', 140));
+// console.log(objectsPhotos);
+// console.log(getRandomArrayElement(userNames));
+
+
+// const maxArrayLength = 25;
+
+const getRandomNumbersInArray = (minValue, maxArrayLength) => {
+  const randomArray = [];
+  let randomArrayValue;
+  const foo = (currentValue) => currentValue === randomArrayValue;
+
+  while (randomArray.length < maxArrayLength) {
+    randomArrayValue = getRandomPositiveInteger(minValue, maxArrayLength);
+
+    if (randomArray.find(foo) >= minValue) {
+      getRandomPositiveInteger(minValue, maxArrayLength);
+    } else {
+      randomArray.push(randomArrayValue);
+    }
+  }
+
+  return randomArray;
+};
+
+const randomArray = getRandomNumbersInArray(1, 25);
+
+console.log(randomArray);
+
+
+//тест с сортировкой
+for (let i = 0; i <= randomArray.length - 2; i++) {
+  let minValue = randomArray[i];
+
+  for (let j = i + 1; j <= randomArray.length - 1; j++) {
+    if (randomArray[j] < minValue) {
+      minValue = randomArray[j];
+      let swap = randomArray[i];
+      randomArray[i] = minValue;
+      randomArray[j] = swap;
+    }
+  }
+}
+
+console.log(randomArray);
 
