547d478dfaac8feffe9113a21ea8840e9e3d24d8
Vladimir <vokar90@yandex.ru>
2022-06-17 15:30:03 +0300

    creating an array of 25 generated photo objects

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// Функцию принес Кекс
-
+// Функция получения рандомных чисел (от Кекса)
 function getRandomPositiveInteger(a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -7,44 +6,45 @@ function getRandomPositiveInteger(a, b) {
   return Math.floor(result);
 }
 
-// Функция для проверки максимальной длины строки Кекстаграм
+// Функция для проверки максимальной длины строки. Кекстаграм
+const checkStringLength = (string, length) => string.length <= length;
 
-function checkStringLength(string, length) {
-  return string.length <= length;
-}
-
-// eslint-disable-next-line no-console
-// console.log(checkStringLength('Some comment', 140));
+checkStringLength('Some comment', 140);
 
 
 // Задание 4. Больше деталей
 
 const userNames = [
-  'Артемий',
-  'Дарья',
-  'Борис',
-  'Анна',
-  'Яна',
-  'Алина',
-  'Матвей',
-  'Виктория',
-  'Тимофей',
-  'Тимур',
-  'Эмилия',
-  'Маргарита',
-  'Илья',
+  'Елизавета',
   'Михаил',
-  'Кристина',
-  'Святослав',
-  'Владислав',
-  'Всеволод',
-  'Егор',
-  'Анатолий',
-  'Александр',
+  'Виктория',
+  'Артём',
+  'Максим',
+  'Кира',
+  'Руслан',
+  'Арсений',
+  'Тимур',
+  'Полина',
+  'Софья',
+  'София',
+  'Ксения',
+  'Варвара',
+  'Макар',
+  'Анастасия',
+  'Сергей',
+  'Мила',
+  'Есения',
+  'Павел',
+  'Кирилл',
+  'Алексей',
+  'Дмитрий',
+  'Владислава',
+  'Тимофей',
   'Константин',
-  'Иван',
-  'Тихон',
-  'Екатерина'
+  'Любовь',
+  'Илья',
+  'Арина',
+  'Таисия',
 ];
 
 const userDescriptions = [
@@ -53,44 +53,24 @@ const userDescriptions = [
   'Моя будущая жена',
   'Круто потусили',
   'Я тут спал',
-  'Говорят js это весело'
+  'Говорят js это весело',
+  'Лучше так не делать',
+  'Твои мечты сбудутся',
+  'Хоп хей лалалей крыша едет - я за ней',
+  'Ковид надоел',
+  'Миру мир'
 ];
 
 const userComments = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const getRandomArrayElement = (array) => array[getRandomPositiveInteger(0, array.length - 1)];
-
-// const getSentencesNumber = () => {}; придумать функцию получения 1 или 2 не повторяющихся предложений.
-
-const generateComment = () => ({
-  'id': '',
-  'avatar': `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
-  'message': '',
-  'name': getRandomArrayElement(userNames),
-});
-
-const generatePhoto = () => ({
-  'id': '',
-  'url': `photos/.jpg`,
-  'description': getRandomArrayElement(userDescriptions),
-  'likes': getRandomPositiveInteger(15, 200),
-  'comments': ''
-});
-
-const objectsPhotos = Array.from({ length: 4 }, generatePhoto);
-
-// eslint-disable-next-line no-console
-// console.log(objectsPhotos);
-// console.log(getRandomArrayElement(userNames));
-
-
+// Функция для получения массива случайных неповторяющихся положительных целых чисел
 const getRandomNumbersInArray = (minValue, maxArrayLength) => {
   const randomArray = [];
   let randomArrayValue = minValue;
@@ -117,7 +97,92 @@ const getRandomNumbersInArray = (minValue, maxArrayLength) => {
   return randomArray;
 };
 
-const randomArray = getRandomNumbersInArray(5, 10);
+const bankUsedRandomNumber = []; // сюда записываются использованные числа
 
-console.log(randomArray);
+// Функция для получения неповторяющегося положительного числа от 0
+const generateRandomNumber = () => {
+  const min = 1;
+  const max = 200; // max должен быть больше чем вызовов этой функции, иначе цикл вайл будет работать бесконечно
+  let randomNumber = getRandomPositiveInteger(min, max);
+  const getСomparison = (currentValue) => currentValue === randomNumber;
+
+  if (bankUsedRandomNumber.find(getСomparison) || bankUsedRandomNumber.find(getСomparison) === 0) { // проверяет есть ли в массиве уже такое число
+    while (bankUsedRandomNumber.find(getСomparison) || bankUsedRandomNumber.find(getСomparison) === 0) { // если есть то генерирует заного, пока не получится уникальное
+      randomNumber = getRandomPositiveInteger(min, max);
+    }
+  }
+
+  bankUsedRandomNumber.push(randomNumber); // добавляет использованное число в массив
+  return randomNumber;
+};
+
+// Функция для получения случайного элемента из массива. Элемент может повторяться.
+const getRandomArrayElement = (array) => array[getRandomPositiveInteger(0, array.length - 1)];
+
+// Функция получения 1 или 2 не повторяющихся сообщений
+const getMessage = () => {
+  const number = getRandomPositiveInteger(1, 2);
+  const firstMessage = getRandomArrayElement(userComments);
+  let secondMessage = getRandomArrayElement(userComments);
+
+  if (secondMessage === firstMessage) {
+    while (secondMessage === firstMessage) {
+      secondMessage = getRandomArrayElement(userComments);
+    }
+  }
+
+  if (number % 2 === 0) {
+    return `${firstMessage} ${secondMessage}`;
+  }
+
+  return firstMessage;
+};
+
+const generateObjectComment = () => ({
+  id: generateRandomNumber(),
+  avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+  message: getMessage(),
+  name: getRandomArrayElement(userNames),
+});
+
+// Генерируем комментарии
+const generateComment = (length) => {
+  const array = [];
+  const randomArray = getRandomNumbersInArray(1, length);
+  let index;
+  for (let i = 0; i < randomArray.length; i++) {
+    index = randomArray[i];
+    array.push(generateObjectComment(index));
+  }
+
+  return array;
+};
+
+const generateObjectPhoto = (idPhoto, pictureNumber) => ({
+  id: idPhoto,
+  url: `photos/${pictureNumber}.jpg`,
+  description: getRandomArrayElement(userDescriptions),
+  likes: getRandomPositiveInteger(15, 200),
+  comments: generateComment(getRandomPositiveInteger(1, 6))
+});
+
+// Генерируем массив фоток
+const generatePhoto = (count) => {
+  const array = [];
+  const randomArray = getRandomNumbersInArray(1, count);
+  const randomArrayForPhoto = getRandomNumbersInArray(1, count);
+  let idPhoto;
+  let pictureNumber;
+  for (let i = 0; i < count; i++) {
+    idPhoto = randomArray[i];
+    pictureNumber = randomArrayForPhoto[i];
+    array.push(generateObjectPhoto(idPhoto, pictureNumber));
+  }
+
+  return array;
+};
+
+const MAX_PHOTO_COUNT = 25; //сколько сделать фотографий
+
+generatePhoto(MAX_PHOTO_COUNT);
 
